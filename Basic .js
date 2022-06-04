59f331a520e46b233a87667dc65d5e7981f28cf7
AshokBavisetti <102503872+AshokBavisetti@users.noreply.github.com>
2022-06-04 11:13:07 +0530

    Add files via upload

---
+++ Basic .js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+
+export default class Basic  extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+          id:'',
+          name:'',
+          author:'',
+      }
+    }
+    changeHandler=(event)=>
+    {
+        this.setState({
+            [event.target.name]:event.target.value
+        })
+    
+    }
+    formHandler=(event)=>
+    {
+axios.posts('http://localhost:4000',this.state)
+.then(response=>
+    {
+console.log('data inserted right')
+ })
+ .catch(error=>{
+    console.log('error')
+    
+ }
+ )
+
+    }
+  render() {
+      const{id,name,author}=this.state
+    return (
+      <div>
+      <form onChange={this.formHandler}>
+<input type="text" name="id" value="{id}" onChange={this.changeHandler}/>
+<input type="text" name="name" value="{name}" onChange={this.changeHandler}/>
+<input type="text" name="author" value="{author}" onChange={this.changeHandler}/>
+<input type="submit" value="submit"/>
+      </form>
+      
+       </div>
+    )
+  }
+}
+z
